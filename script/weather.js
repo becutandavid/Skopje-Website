61ccd2f2a8750de7fbc9316feaa4183a3074aa38
cogscape <qlimax.republic@gmail.com>
2016-05-26 08:08:54 +0200

    Update
    -Created proper events.html;
    -Changed photos;
    -Began using glyphicons;
    -Javascript Weather Widget finnished;
    -Fixed responsive glitch with navbar-brand at bellow 330px;
    -Tidied code... Improved performance;

--- script/weather.js
+++ script/weather.js
@@ -1,11 +1,21 @@
 // Docs at http://simpleweatherjs.com
 $(document).ready(function() {
   $.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
-    unit: 'f',
+    location: '',
+    woeid: '482940',
+    unit: 'c',
     success: function(weather) {
-      html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+      html = '<p class="weather-date">Today</p>';
+      html += '<p class="weather-temp">'+weather.forecast[0].low
+           +'/'+weather.forecast[0].high+'&deg;'
+             +weather.units.temp+'</p><br>';
+      
+      for(var i=1;i<5;i++) {
+      html += '<p class="weather-date">'+weather.forecast[i].date+'</p>';
+      html += '<p class="weather-temp">'+weather.forecast[i].low
+           +'/'+weather.forecast[i].high+'&deg;'
+             +weather.units.temp+'</p><br>';
+      }
   
       $("#weather").html(html);
     },
